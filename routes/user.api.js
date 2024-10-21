4870bd140500f19f30d545e196d492a3947299aa
김예린 <yerin.3321@daum.net>
2024-10-21 22:52:11 +0900

    token verify

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controller/auth.controller");
 const userController = require("../controller/user.controller");
 
 // 1.회원가입 endpoint
 router.post("/", userController.createUser);
 router.post("/login", userController.loginWithEmail);
+router.get("/me", authController.authenticate, userController.getUser);
 
 module.exports = router;
